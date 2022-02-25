cf29f9c67599cc399011ed26b339552d9b73783a
Andrew <andrew.morgan@e.org>
2022-02-25 17:15:36 +0000

    Added map icons

--- src/components/Map/Map.jsx
+++ src/components/Map/Map.jsx
@@ -6,7 +6,7 @@ import Rating from "@material-ui/lab/Rating";
 import useStyles from "./styles.js";
 
 const Map = ({ setCoordinates, setBounds, coordinates, places }) => {
-  const isMobile = useMediaQuery("(min-width:600px)");
+  const isDesktop = useMediaQuery("(min-width:600px)");
   const classes = useStyles();
 
   return (
@@ -24,8 +24,37 @@ const Map = ({ setCoordinates, setBounds, coordinates, places }) => {
         }}
         onChildClick={() => {}}
       >
-        {places?.map((place) => (
-          <div></div>
+        {places?.map((place, i) => (
+          <div
+            className={classes.markerContainer}
+            lat={Number(place.latitude)}
+            lng={Number(place.longitude)}
+            key={i}
+          >
+            {!isDesktop ? (
+              <LocationOnOutlinedIcon color="primary" fontSize="large" />
+            ) : (
+              <Paper elevation={3} className={classes.paper}>
+                <Typography
+                  className={classes.typography}
+                  variant="subtitle2"
+                  gutterBottom
+                >
+                  {place.name}
+                </Typography>
+                <img
+                  src={
+                    place.photo
+                      ? place.photo.images.large.url
+                      : "https://www.foodserviceandhospitality.com/wp-content/uploads/2016/09/Restaurant-Placeholder-001.jpg"
+                  }
+                  alt={place.name}
+                  className={classes.pointer}
+                />
+                <Rating size="small" value={Number(place.rating)} readOnly />
+              </Paper>
+            )}
+          </div>
         ))}
       </GoogleMapReact>
     </div>

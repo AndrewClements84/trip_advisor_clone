a80b2f37f6575c3717a480b8aa7ccb56cc75fce6
Andrew <andrew.morgan@e.org>
2022-02-25 16:53:46 +0000

    Added map and list elements and implmentations

--- src/components/Map/Map.jsx
+++ src/components/Map/Map.jsx
@@ -1,5 +1,35 @@
-const Map = () => {
-  return <h1>Map</h1>;
+import React from "react";
+import GoogleMapReact from "google-map-react";
+import { Paper, Typography, useMediaQuery } from "@material-ui/core";
+import LocationOnOutlinedIcon from "@material-ui/icons/LocationOnOutlined";
+import Rating from "@material-ui/lab/Rating";
+import useStyles from "./styles.js";
+
+const Map = ({ setCoordinates, setBounds, coordinates, places }) => {
+  const isMobile = useMediaQuery("(min-width:600px)");
+  const classes = useStyles();
+
+  return (
+    <div className={classes.mapContainer}>
+      <GoogleMapReact
+        bootstrapURLKeys={{ key: "AIzaSyDHc2DTTh9Ne5PpSS8BuadSf61n8KvZU9Q" }}
+        defaultCenter={coordinates}
+        center={coordinates}
+        defaultZoom={14}
+        margin={[50, 50, 50, 50]}
+        options={{}}
+        onChange={(e) => {
+          setCoordinates({ lat: e.center.lat, lng: e.center.lng });
+          setBounds({ ne: e.marginBounds.ne, sw: e.marginBounds.sw });
+        }}
+        onChildClick={() => {}}
+      >
+        {places?.map((place) => (
+          <div></div>
+        ))}
+      </GoogleMapReact>
+    </div>
+  );
 };
 
 export default Map;
